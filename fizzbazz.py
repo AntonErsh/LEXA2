d67761fca84e2e4f96db8a43ab5d07a3c5f7a3cf
AntonErsh <ershov.anton4ik@yandex.ru>
2025-02-25 19:58:04 +0300

    Add get_info

--- fizzbazz.py
+++ fizzbazz.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+def get_info(name: str) -> str:
+    """
+    IDI NAXUI
+    :param name: Your name
+    :return: Your name gigi
+    """
+    if name == "LEXA":
+        yield "You are Pidor"
+    else:
+        yield "You're blessed"
+
+
+print(get_info('LEXA'))
+print(get_info('Burp'))
